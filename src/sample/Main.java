6d354958e0b1e5d716c6fd18789787cf0e30d339
Tom Barnes <ruralmyth@gmail.com>
2016-12-12 09:39:24 +0000

    basic invoice details form completed with for loops

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,27 +1,36 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main extends Application {
+
+    private Scene newInvoiceScene;
+    private Scene customerScene;
+    private Scene mainMenuScene;
+    private Scene settingsScene;
+    private Scene invoiceDetailsScene;
+    private static final Color CARBON = Color.valueOf("A9A9A9");
+    private static final Color WATERMELON = Color.valueOf("FF3B3F");
+    private static final Color GRAIN = Color.valueOf("D7CEC7");
+    private static final Color OXBLOOD = Color.valueOf("76323F");
+    private static final Color BLACKBOARD = Color.valueOf("565656");
 
     @Override
     public void start(Stage primaryStage) {
@@ -32,53 +41,373 @@ public class Main extends Application {
 //
 //        StackPane root = new StackPane();
 //        root.getChildren().add(btn);
+//
+//        Scene buttonTestScene = new Scene(root, 500, 700);
 
-//        Scene scene = new Scene(root, 300, 250);
+        //
+        //
+        //
+        // MAIN MENU
+        //
 
-        GridPane grid = new GridPane();
-        grid.setAlignment(Pos.CENTER);
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new Insets(25, 25, 25, 25));
+        GridPane mainMenuGrid = new GridPane();
+        mainMenuGrid.setAlignment(Pos.CENTER);
+        mainMenuGrid.setHgap(10);
+        mainMenuGrid.setVgap(10);
+        mainMenuGrid.setPadding(new Insets(25, 25, 25, 25));
 
-        Scene scene = new Scene(grid, 300, 275);
+        Text bannerTitle = new Text("J A V O I C E");
+        bannerTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        bannerTitle.setFill(OXBLOOD);
+        mainMenuGrid.add(bannerTitle, 0, 0, 2, 1);
 
-        Text scenetitle = new Text("New customer");
-        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-        grid.add(scenetitle, 0, 0, 2, 1);
+        Text mainMenuTitle = new Text("Main menu");
+        mainMenuTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        mainMenuTitle.setFill(BLACKBOARD);
+        mainMenuGrid.add(mainMenuTitle, 0, 5, 2, 1);
+
+//        Button dummyFocusButton = new Button();
+//        dummyFocusButton.requestFocus();
+//        dummyFocusButton.setVisible(false);
+//        mainMenuGrid.add(dummyFocusButton, 0, 16);
+
+        Button mainNewInvoice = new Button();
+        mainNewInvoice.setText("New invoice");
+        mainNewInvoice.setTextFill(BLACKBOARD);
+        mainNewInvoice.setMinWidth(200);
+        mainNewInvoice.setStyle("-fx-focus-color: transparent;");
+        mainNewInvoice.setOnAction(event -> primaryStage.setScene(newInvoiceScene));
+        mainMenuGrid.add(mainNewInvoice, 0, 6);
+
+        Button mainNewCustomer = new Button();
+        mainNewCustomer.setText("New customer");
+        mainNewCustomer.setTextFill(BLACKBOARD);
+        mainNewCustomer.setMinWidth(200);
+        mainNewCustomer.setStyle("-fx-focus-color: transparent;");
+        mainNewCustomer.setOnAction(event -> primaryStage.setScene(customerScene));
+        mainMenuGrid.add(mainNewCustomer, 0, 7);
+
+        Button mainSettings = new Button();
+        mainSettings.setText("Settings");
+        mainSettings.setTextFill(BLACKBOARD);
+        mainSettings.setMinWidth(200);
+        mainSettings.setStyle("-fx-focus-color: transparent;");
+        mainSettings.setOnAction(event -> primaryStage.setScene(settingsScene));
+        mainMenuGrid.add(mainSettings, 0, 8);
+
+        Label copyright = new Label(String.format("© %s  Tom Barnes", copyrightYears()));
+        copyright.setTextFill(OXBLOOD);
+        mainMenuGrid.add(copyright, 0, 15);
+
+        mainMenuScene = new Scene(mainMenuGrid, 500, 700);
+        mainMenuScene.getStylesheets().add("sample/javoice.css");
+        bannerTitle.requestFocus();
+
+        //
+        //
+        //
+        // NEW INVOICE SCENE
+        //
+        GridPane newInvoiceGrid = new GridPane();
+        newInvoiceGrid.setAlignment(Pos.CENTER);
+        newInvoiceGrid.setHgap(10);
+        newInvoiceGrid.setVgap(10);
+        newInvoiceGrid.setPadding(new Insets(25, 25, 25, 25));
+
+        Text invoiceSceneTitle = new Text("New invoice");
+        invoiceSceneTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        newInvoiceGrid.add(invoiceSceneTitle, 0, 0, 2, 1);
+
+        Label customerSearchLabel = new Label("Search for existing customer:");
+        newInvoiceGrid.add(customerSearchLabel, 0, 1);
+
+        TextField customerSearch = new TextField();
+        newInvoiceGrid.add(customerSearch, 1, 1);
+
+        Button searchCustomerButton = new Button();
+        searchCustomerButton.setText("Search");
+        searchCustomerButton.setOnAction(event -> primaryStage.setScene(invoiceDetailsScene));
+        newInvoiceGrid.add(searchCustomerButton, 2, 1);
+
+        Label orLabel = new Label("- OR -");
+        newInvoiceGrid.add(orLabel, 0, 2);
+
+        Button newCustomerButton = new Button();
+        newCustomerButton.setText("Add new customer");
+        newCustomerButton.setOnAction(event -> primaryStage.setScene(customerScene));
+        newInvoiceGrid.add(newCustomerButton, 0, 3);
+
+//        Label quantity = new Label("Quantity");
+//        newInvoiceGrid.add(quantity, 0, 2);
+//
+//        Label description = new Label("Description");
+//        newInvoiceGrid.add(description, 1, 2);
+//
+//        Label unitPrice = new Label("Unit price");
+//        newInvoiceGrid.add(unitPrice, 2, 2);
+
+        newInvoiceScene = new Scene(newInvoiceGrid, 500, 700);
+
+        Button mainFromInvoice = new Button();
+        mainFromInvoice.setText("Main menu");
+        mainFromInvoice.setOnAction(event -> primaryStage.setScene(mainMenuScene));
+        newInvoiceGrid.add(mainFromInvoice, 0, 7);
+        invoiceSceneTitle.requestFocus();
+
+        //
+        //
+        //
+        // INVOICE DETAILS SCENE
+        //
+        GridPane invoiceDetailsGrid = new GridPane();
+        invoiceDetailsGrid.setAlignment(Pos.CENTER);
+        invoiceDetailsGrid.setHgap(10);
+        invoiceDetailsGrid.setVgap(10);
+        invoiceDetailsGrid.setPadding(new Insets(25, 25, 25, 25));
+
+        Text invoiceDetailsTitle = new Text("Invoice details:");
+        invoiceDetailsTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        invoiceDetailsGrid.add(invoiceDetailsTitle, 0, 0, 2, 1);
+
+        Label nameLabel = new Label("Name:");
+        invoiceDetailsGrid.add(nameLabel, 1, 2);
+
+        TextField nameField = new TextField();
+        invoiceDetailsGrid.add(nameField, 1, 3);
+
+        Label addressOne = new Label("Address (1):");
+        invoiceDetailsGrid.add(addressOne, 1, 4);
+
+        TextField addressField = new TextField();
+        GridPane.setColumnSpan(addressField, 2);
+        invoiceDetailsGrid.add(addressField, 1, 5);
+
+        Label addressTwo = new Label("Address (2):");
+        invoiceDetailsGrid.add(addressTwo, 1, 6);
+
+        TextField addressTwoField = new TextField();
+        invoiceDetailsGrid.add(addressTwoField, 1, 7);
+
+        Label postcodeLabel = new Label("Postcode:");
+        invoiceDetailsGrid.add(postcodeLabel, 2, 6);
+
+        TextField postcodeField = new TextField();
+        invoiceDetailsGrid.add(postcodeField, 2, 7);
+
+        Label dateLabel = new Label("Date:");
+        invoiceDetailsGrid.add(dateLabel, 4, 2);
+
+        TextField dateField = new TextField();
+        invoiceDetailsGrid.add(dateField, 4, 3);
+
+        Label orderNumberLabel = new Label("Order Number:");
+        invoiceDetailsGrid.add(orderNumberLabel, 4, 4);
+
+        TextField orderField = new TextField();
+        invoiceDetailsGrid.add(orderField, 4, 5);
+
+        Label accountCodeLabel = new Label("Account code:");
+        invoiceDetailsGrid.add(accountCodeLabel, 4, 6);
+
+        TextField accountCodeField = new TextField();
+        invoiceDetailsGrid.add(accountCodeField, 4, 7);
+
+        Label quantity = new Label("Quantity");
+        invoiceDetailsGrid.add(quantity, 0, 12);
+
+        Label description = new Label("Description");
+        GridPane.setColumnSpan(description, 2);;
+        invoiceDetailsGrid.add(description, 1, 12);
+
+        Label unitPrice = new Label("Unit price");
+        invoiceDetailsGrid.add(unitPrice, 3, 12);
+
+        Label total = new Label("Total");
+        invoiceDetailsGrid.add(total, 4, 12);
+
+        List<TextField> quantitiyFieldList = new ArrayList<>();
+        for (int i = 0; i < 16; i++) {
+            quantitiyFieldList.add(new TextField());
+            invoiceDetailsGrid.add(quantitiyFieldList.get(i), 0, 13 + i);
+        }
+
+        List<TextField> descriptionFieldList = new ArrayList<>();
+        for (int i = 0; i < 16; i++) {
+            descriptionFieldList.add(new TextField());
+            GridPane.setColumnSpan(descriptionFieldList.get(i), 2);
+            invoiceDetailsGrid.add(descriptionFieldList.get(i), 1, 13 + i);
+        }
+
+        List<TextField> unitPriceList = new ArrayList<>();
+        for (int i = 0; i < 16; i++) {
+            unitPriceList.add(new TextField());
+            invoiceDetailsGrid.add(unitPriceList.get(i), 3, 13 + i);
+        }
+
+        List<TextField> totalList = new ArrayList<>();
+        for (int i = 0; i < 16; i++) {
+            totalList.add(new TextField());
+            invoiceDetailsGrid.add(totalList.get(i), 4, 13 + i);
+        }
+
+        Button invoiceSubmitButton = new Button();
+        invoiceSubmitButton.setText("Submit");
+        invoiceSubmitButton.setOnAction(event -> System.out.println("INVOICE SUBMITTED!"));
+        invoiceDetailsGrid.add(invoiceSubmitButton, 0, 30);
+
+        ScrollPane invoiceDetailsScroll = new ScrollPane(invoiceDetailsGrid);
+
+        invoiceDetailsScene = new Scene(invoiceDetailsScroll, 500, 700);
+
+        //
+        //
+        //
+        // NEW CUSTOMER SCENE
+        //
+
+        GridPane addCustomerGrid = new GridPane();
+        addCustomerGrid.setAlignment(Pos.CENTER);
+        addCustomerGrid.setHgap(10);
+        addCustomerGrid.setVgap(10);
+        addCustomerGrid.setPadding(new Insets(25, 25, 25, 25));
+
+        customerScene = new Scene(addCustomerGrid, 500, 700);
+
+        Text customerSceneTitle = new Text("New customer");
+        customerSceneTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        addCustomerGrid.add(customerSceneTitle, 0, 0, 2, 1);
 
         Label customerName = new Label("Name");
-        grid.add(customerName, 0, 1);
+        addCustomerGrid.add(customerName, 0, 1);
 
         TextField customerNameField = new TextField();
-        grid.add(customerNameField, 1, 1);
+        addCustomerGrid.add(customerNameField, 1, 1);
 
-        Label addressOne = new Label("Address (1)");
-        grid.add(addressOne, 0, 2);
+        Label customerAddressOne = new Label("Address (1)");
+        addCustomerGrid.add(customerAddressOne, 0, 2);
 
         TextField customerAddressOneField = new TextField();
-        grid.add(customerAddressOneField, 1, 2);
+        addCustomerGrid.add(customerAddressOneField, 1, 2);
 
-        Label customerAddressTwoField = new Label("Address (2)");
-        grid.add(customerAddressTwoField, 0, 3);
+        Label customerAddressTwo = new Label("Address (2)");
+        addCustomerGrid.add(customerAddressTwo, 0, 3);
+
+        TextField customerAddressTwoField = new TextField();
+        addCustomerGrid.add(customerAddressTwoField, 1, 3);
+
+        Label customerPostcode = new Label("Postcode");
+        addCustomerGrid.add(customerPostcode, 0, 4);
 
         TextField customerPostcodeField = new TextField();
-        grid.add(customerPostcodeField, 1, 3);
+        addCustomerGrid.add(customerPostcodeField, 1, 4);
 
         Label customerPhoneNum = new Label("Phone number:");
-        grid.add(customerPhoneNum, 0, 4);
+        addCustomerGrid.add(customerPhoneNum, 0, 5);
 
         TextField customerPhoneField = new TextField();
-        grid.add(customerPhoneField, 1, 4);
+        addCustomerGrid.add(customerPhoneField, 1, 5);
 
-        Button btn = new Button();
-        btn.setText("Add customer");
-        btn.setOnAction(event -> System.out.println("...adding customer"));
-        grid.add(btn, 0, 5);
+        Button submitCustomerBtn = new Button();
+        submitCustomerBtn.setText("Add customer");
+        submitCustomerBtn.setOnAction(event -> printCustomerDetails(
+                customerNameField,
+                customerAddressOneField,
+                customerAddressTwoField,
+                customerPostcodeField,
+                customerPhoneField
+        ));
+        addCustomerGrid.add(submitCustomerBtn, 0, 6);
+
+        Button mainFromCustomer = new Button();
+        mainFromCustomer.setText("Main menu");
+        mainFromCustomer.setOnAction(event -> primaryStage.setScene(mainMenuScene));
+        addCustomerGrid.add(mainFromCustomer, 0, 7);
+
+        //
+        //
+        //
+        // SETTINGS
+        //
+
+        GridPane settingsGrid = new GridPane();
+        settingsGrid.setAlignment(Pos.CENTER);
+        settingsGrid.setHgap(10);
+        settingsGrid.setVgap(10);
+        settingsGrid.setPadding(new Insets(25, 25, 25, 25));
+
+        Text settingsTitle = new Text("Settings");
+        settingsTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        settingsGrid.add(settingsTitle, 0, 0, 2, 1);
+
+        Label invoiceFileTemplateLabel = new Label("Invoice Template path:");
+        settingsGrid.add(invoiceFileTemplateLabel, 0, 1);
+
+        TextField invoiceFileTemplatePath = new TextField();
+        settingsGrid.add(invoiceFileTemplatePath, 1, 1);
+
+        Label invoiceFileOutputLabel = new Label("Invoice output path:");
+        settingsGrid.add(invoiceFileOutputLabel, 0, 2);
+
+        TextField invoiceFileOutputPath = new TextField();
+        settingsGrid.add(invoiceFileOutputPath, 1, 2);
+
+        Label salesLedgerOutputLabel = new Label("Sales ledger output path:");
+        settingsGrid.add(salesLedgerOutputLabel, 0, 3);
+
+        TextField salesLedgerOutputPath = new TextField();
+        settingsGrid.add(salesLedgerOutputPath, 1, 3);
+
+        Label customerDataLabel = new Label("Customer data path:");
+        settingsGrid.add(customerDataLabel, 0, 4);
+
+        TextField customerDataPath = new TextField();
+        settingsGrid.add(customerDataPath, 1, 4);
+
+        Button updateSettings = new Button();
+        updateSettings.setText("Update settings");
+        updateSettings.setOnAction(event -> System.out.println("settings updated..."));
+        settingsGrid.add(updateSettings, 0, 6);
+
+        Button mainFromSettings = new Button();
+        mainFromSettings.setText("Main menu");
+        mainFromSettings.setOnAction(event -> primaryStage.setScene(mainMenuScene));
+        settingsGrid.add(mainFromSettings, 0, 8);
+
+        settingsScene = new Scene(settingsGrid, 500, 700);
+
+        //
+        //
+        // SET THE STAGE...
+        //
 
         primaryStage.setTitle("Javoice");
-        primaryStage.setScene(scene);
+        primaryStage.setScene(mainMenuScene);
         primaryStage.show();
+    }
+
+//    private void switchScene(Stage primaryStage) {
+
+    //    }
+//        }
+//            primaryStage.setScene(newInvoiceScene);
+//        } else {
+//            primaryStage.setScene(customerScene);
+//        if (primaryStage.getScene() == newInvoiceScene) {
+
+    private String copyrightYears() {
+        if (LocalDate.now().getYear() == 2016) {
+            return "2016";
+        }
+        return String.format("2016-%s", LocalDate.now().getYear());
+    }
+
+    private void printCustomerDetails(TextField nameField, TextField customerAddressOneField, TextField customerAddressTwoField, TextField customerPostcodeField, TextField customerPhoneField) {
+        System.out.println(String.format("...adding customer %s, from %s %s %s, ph. %s",
+                nameField.getText(),
+                customerAddressOneField.getText(),
+                customerAddressTwoField.getText(),
+                customerPostcodeField.getText(),
+                customerPhoneField.getText()));
     }
 
 //    @Override
